64160ddddf14e68ce85138924d218f935e92368c
palaram29 <135940827+palaram29@users.noreply.github.com>
2024-04-22 16:07:06 +0530

    Update CartServiceImpl.java
    This java code is implementation of cartservice.
    
    This file consists of "addProductToCart method" and "getAllCartItems method"
    addProductToCart method consist,
         Adds a product to the cart, updates quantity if it's already there, handles cases where the available quantity is less than requested, and returns a status message.
    
    getAllCartItems method
        Retrieves all items in the user's cart, constructs objects with user ID, product ID, and quantity, and returns a list of these objects.

--- green_life/Impl/CartServiceImpl.java
+++ green_life/Impl/CartServiceImpl.java
@@ -1,1 +1,123 @@
+package com.shashi.service.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.shashi.beans.CartBean;
+import com.shashi.beans.DemandBean;
+import com.shashi.beans.ProductBean;
+import com.shashi.service.CartService;
+import com.shashi.utility.DBUtil;
+
+public class CartServiceImpl implements CartService {
+
+	@Override
+	public String addProductToCart(String userId, String prodId, int prodQty) {
+		String status = "Failed to Add into Cart";
+
+		Connection con = DBUtil.provideConnection();
+
+		PreparedStatement ps = null;
+		PreparedStatement ps2 = null;
+		ResultSet rs = null;
+
+		try {
+
+			ps = con.prepareStatement("select * from usercart where username=? and prodid=?");
+
+			ps.setString(1, userId);
+			ps.setString(2, prodId);
+
+			rs = ps.executeQuery();
+
+			if (rs.next()) {
+
+				int cartQuantity = rs.getInt("quantity");
+
+				ProductBean product = new ProductServiceImpl().getProductDetails(prodId);
+
+				int availableQty = product.getProdQuantity();
+
+				prodQty += cartQuantity;
+				//
+				if (availableQty < prodQty) {
+
+					status = updateProductToCart(userId, prodId, availableQty);
+
+					status = "Only " + availableQty + " no of " + product.getProdName()
+							+ " are available in the shop! So we are adding only " + availableQty
+							+ " no of that item into Your Cart" + "";
+
+					DemandBean demandBean = new DemandBean(userId, product.getProdId(), prodQty - availableQty);
+
+					DemandServiceImpl demand = new DemandServiceImpl();
+
+					boolean flag = demand.addProduct(demandBean);
+
+					if (flag)
+						status += "<br/>Later, We Will Mail You when " + product.getProdName()
+								+ " will be available into the Store!";
+
+				} else {
+					status = updateProductToCart(userId, prodId, prodQty);
+
+				}
+			}
+
+		} catch (SQLException e) {
+			status = "Error: " + e.getMessage();
+			e.printStackTrace();
+		}
+
+		DBUtil.closeConnection(con);
+		DBUtil.closeConnection(ps);
+		DBUtil.closeConnection(rs);
+		DBUtil.closeConnection(ps2);
+
+		return status;
+	}
+
+	@Override
+	public List<CartBean> getAllCartItems(String userId) {
+		List<CartBean> items = new ArrayList<CartBean>();
+
+		Connection con = DBUtil.provideConnection();
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+
+			ps = con.prepareStatement("select * from usercart where username=?");
+
+			ps.setString(1, userId);
+
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				CartBean cart = new CartBean();
+
+				cart.setUserId(rs.getString("username"));
+				cart.setProdId(rs.getString("prodid"));
+				cart.setQuantity(Integer.parseInt(rs.getString("quantity")));
+
+				items.add(cart);
+
+			}
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+		DBUtil.closeConnection(con);
+		DBUtil.closeConnection(ps);
+		DBUtil.closeConnection(rs);
+
+		return items;
+	}
 
