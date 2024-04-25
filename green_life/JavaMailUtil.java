d8089481f295eaf6e7477c788742e3b1f4aefb14
Nithujayan U <159274930+NithujayanU@users.noreply.github.com>
2024-04-25 19:45:42 +0530

    Update JavaMailUtil.java
    In here i have created two methods for preparing message and sending the message.
    In prepareMessage() method,
      It takes sender email address and recipient address as a parameter.
      Then it setting the sender mail address using setForm() and recipient mail addres using setReciepiet().
      Finally it sets the subject of the mail.
    
    In sendMail() method,
      This method is responsible for sending an email.
      It sets up properties required for sending emails, such as SMTP host, authentication, and port.
      It retrieves the sender's email credentials (emailId and passWord) from a properties file named "application".
      It creates a session using the properties and authenticates the sender using the provided credentials.
      It calls the prepareMessage() method to create the email message, passing the session, sender's email address, and recipient's email address as parameters.
      It sends the prepared message using the Transport class.
      Finally it prints a message to the confirming that the email was sent successfully.

--- green_life/JavaMailUtil.java
+++ green_life/JavaMailUtil.java
@@ -51,4 +51,60 @@ public class JavaMailUtil {
 
 	}
 
+	private static Message prepareMessage(Session session, String myAccountEmail, String recipientEmail) {
+
+		try {
+
+			Message message = new MimeMessage(session);
+
+			message.setFrom(new InternetAddress(myAccountEmail));
+			message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipientEmail));
+			message.setSubject("Welcome to Green Life");
+			message.setText("Hey! " + recipientEmail + ", Thanks  for Signing Up with us!");
+			return message;
+
+		} catch (Exception exception) {
+			Logger.getLogger(JavaMailUtil.class.getName()).log(Level.SEVERE, null, exception);
+		}
+		return null;
+
+	}
+
+	protected static void sendMail(String recipient, String subject, String htmlTextMessage) throws MessagingException {
+
+		System.out.println("Preparing to send Mail");
+		Properties properties = new Properties();
+		String host = "smtp.gmail.com";
+		properties.put("mail.smtp.host", host);
+		properties.put("mail.transport.protocol", "smtp");
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.starttls.enable", "true");
+		properties.put("mail.smtp.port", "587");
+
+		ResourceBundle rb = ResourceBundle.getBundle("application");
+
+		String emailId = rb.getString("mailer.email");
+		String passWord = rb.getString("mailer.password");
+
+		properties.put("mail.user", emailId);
+		properties.put("mail.password", passWord);
+
+		Session session = Session.getInstance(properties, new Authenticator() {
+
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(emailId, passWord);
+			}
+
+		});
+
+		Message message = prepareMessage(session, emailId, recipient, subject, htmlTextMessage);
+
+		Transport.send(message);
+
+		System.out.println("Message Sent Successfully!");
+
+	}
+	
+
 }
