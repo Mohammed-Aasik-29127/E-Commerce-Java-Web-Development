329c3f8281f1e225d3f95a9e687b46ae3387467f
Shakeef <140883332+mdShakeef@users.noreply.github.com>
2024-04-25 19:52:47 +0530

    Update UpdateToCart.java
    The rest of the index saved yesterday and this is Overall, it appears that this servlet manages to update the user's shopping basket.

--- src/UpdateToCart.java
+++ src/UpdateToCart.java
@@ -39,3 +39,64 @@ public class UpdateToCart extends HttpServlet {
 
 			response.sendRedirect("login.jsp?message=Session Expired, Login Again!!");
 		}
+// login Check Successfull
+
+		String userId = userName;
+		String prodId = request.getParameter("pid");
+		int pQty = Integer.parseInt(request.getParameter("pqty"));
+
+		CartServiceImpl cart = new CartServiceImpl();
+
+		ProductServiceImpl productDao = new ProductServiceImpl();
+
+		ProductBean product = productDao.getProductDetails(prodId);
+
+		int availableQty = product.getProdQuantity();
+
+		PrintWriter pw = response.getWriter();
+
+		response.setContentType("text/html");
+
+		if (availableQty < pQty) {
+
+			String status = cart.updateProductToCart(userId, prodId, availableQty);
+
+			status = "Only " + availableQty + " no of " + product.getProdName()
+					+ " are available in the shop! So we are adding only " + availableQty + " products into Your Cart"
+					+ "";
+
+			DemandBean demandBean = new DemandBean(userName, product.getProdId(), pQty - availableQty);
+
+			DemandServiceImpl demand = new DemandServiceImpl();
+
+			boolean flag = demand.addProduct(demandBean);
+
+			if (flag)
+				status += "<br/>Later, We Will Mail You when " + product.getProdName()
+						+ " will be available into the Store!";
+
+			RequestDispatcher rd = request.getRequestDispatcher("cartDetails.jsp");
+
+			rd.include(request, response);
+
+			pw.println("<script>document.getElementById('message').innerHTML='" + status + "'</script>");
+
+		} else {
+			String status = cart.updateProductToCart(userId, prodId, pQty);
+
+			RequestDispatcher rd = request.getRequestDispatcher("cartDetails.jsp");
+
+			rd.include(request, response);
+
+			pw.println("<script>document.getElementById('message').innerHTML='" + status + "'</script>");
+		}
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
