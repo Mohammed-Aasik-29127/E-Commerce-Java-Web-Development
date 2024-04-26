78b17bdbe52f1250a977bf3a32d4d6fcf0c96905
palaram29 <135940827+palaram29@users.noreply.github.com>
2024-04-26 10:46:56 +0530

    Update OrderServiceImpl.java
    This OrderServiceImpl class implements the OrderService interface.
    
    This method takes the username and the paid amount as input and attempts to place an order based on the user's cart items. It returns a success message ("Order Placed Successfully!") if successful, or a failure message ("Order Placement Failed!")
    
    It retrieves all the items in the user's cart using the “getAllCartItems” method from “CartServiceImpl”. If the cart is empty, the method returns with the status indicating failure.
    
    A “TransactionBean” object is created with the user's username and the paid amount.
    
    For each item in the cart it calculates the total amount and creates an “OrderBean” object and attempts to add the order. If any step fails (adding order, removing product from the cart, or updating product quantity) the loop breaks and the method returns with the status indicating failure.
    
    If all orders are successfully processed, it attempts to add the transaction. If successful it sends a transaction success message to the user and updates the status to "Order Placed Successfully!".

--- green_life/Impl/OrderServiceImpl.java
+++ green_life/Impl/OrderServiceImpl.java
@@ -1,1 +1,76 @@
+package green_life.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import CartBean;
+import OrderBean;
+import OrderDetails;
+import TransactionBean;
+import OrderService;
+import DBUtil;
+import MailMessage;
+
+public class OrderServiceImpl implements OrderService {
+
+	@Override
+	public String paymentSuccess(String userName, double paidAmount) {
+		String status = "Order Placement Failed!";
+
+		List<CartBean> cartItems = new ArrayList<CartBean>();
+		cartItems = new CartServiceImpl().getAllCartItems(userName);
+
+		if (cartItems.size() == 0)
+			return status;
+
+		TransactionBean transaction = new TransactionBean(userName, paidAmount);
+		boolean ordered = false;
+
+		String transactionId = transaction.getTransactionId();
+
+		// System.out.println("Transaction: "+transaction.getTransactionId()+"
+		// "+transaction.getTransAmount()+" "+transaction.getUserName()+"
+		// "+transaction.getTransDateTime());
+
+		for (CartBean item : cartItems) {
+
+			double amount = new ProductServiceImpl().getProductPrice(item.getProdId()) * item.getQuantity();
+
+			OrderBean order = new OrderBean(transactionId, item.getProdId(), item.getQuantity(), amount);
+
+			ordered = addOrder(order);
+			if (!ordered)
+				break;
+			else {
+				ordered = new CartServiceImpl().removeAProduct(item.getUserId(), item.getProdId());
+			}
+
+			if (!ordered)
+				break;
+			else
+				ordered = new ProductServiceImpl().sellNProduct(item.getProdId(), item.getQuantity());
+
+			if (!ordered)
+				break;
+		}
+
+		if (ordered) {
+			ordered = new OrderServiceImpl().addTransaction(transaction);
+			if (ordered) {
+
+				MailMessage.transactionSuccess(userName, new UserServiceImpl().getFName(userName),
+						transaction.getTransactionId(), transaction.getTransAmount());
+
+				status = "Order Placed Successfully!";
+			}
+		}
+
+		return status;
+	}
+
+  
+}
