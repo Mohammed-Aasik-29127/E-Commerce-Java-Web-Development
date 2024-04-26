d85b6460c047d067218c312b835a8ee6da6c4a8e
Nusky Ahamed <135939524+Nuskyn@users.noreply.github.com>
2024-04-26 17:28:57 +0530

    Update mysql_query.sql
    addwd more tables

--- sql_query/mysql_query.sql
+++ sql_query/mysql_query.sql
@@ -43,3 +43,79 @@ CREATE TABLE IF NOT EXISTS `shopping-cart`.`orders` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
+
+-- -----------------------------------------------------
+-- Table `shopping-cart`.`user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `shopping-cart`.`user` ;
+
+CREATE TABLE IF NOT EXISTS `shopping-cart`.`user` (
+  `email` VARCHAR(60) NOT NULL,
+  `name` VARCHAR(30) NULL DEFAULT NULL,
+  `mobile` BIGINT NULL DEFAULT NULL,
+  `address` VARCHAR(250) NULL DEFAULT NULL,
+  `pincode` INT NULL DEFAULT NULL,
+  `password` VARCHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`email`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `shopping-cart`.`transactions`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `shopping-cart`.`transactions` ;
+
+CREATE TABLE IF NOT EXISTS `shopping-cart`.`transactions` (
+  `transid` VARCHAR(45) NOT NULL,
+  `username` VARCHAR(60) NULL DEFAULT NULL,
+  `time` DATETIME NULL DEFAULT NULL,
+  `amount` DECIMAL(10,2) NULL DEFAULT NULL,
+  PRIMARY KEY (`transid`),
+  INDEX `truserid_idx` (`username` ASC) VISIBLE,
+  CONSTRAINT `truserid`
+    FOREIGN KEY (`username`)
+    REFERENCES `shopping-cart`.`user` (`email`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `transorderid`
+    FOREIGN KEY (`transid`)
+    REFERENCES `shopping-cart`.`orders` (`orderid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `shopping-cart`.`user_demand`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `shopping-cart`.`user_demand` ;
+
+CREATE TABLE IF NOT EXISTS `shopping-cart`.`user_demand` (
+  `username` VARCHAR(60) NOT NULL,
+  `prodid` VARCHAR(45) NOT NULL,
+  `quantity` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`username`, `prodid`),
+  INDEX `prodid_idx` (`prodid` ASC) VISIBLE,
+  CONSTRAINT `userdemailemail`
+    FOREIGN KEY (`username`)
+    REFERENCES `shopping-cart`.`user` (`email`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `prodid`
+    FOREIGN KEY (`prodid`)
+    REFERENCES `shopping-cart`.`product` (`pid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+
+
+
+
