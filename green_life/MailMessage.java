94e4c97c830b59f23268490d7ea5248baacfd380
Nithujayan U <159274930+NithujayanU@users.noreply.github.com>
2024-04-28 00:08:25 +0530

    Update MailMessage.java
    This code sends an email to a user who has successfully registered on the "Green Life" platform, welcoming them and providing information about the platform's services and contact details.
      This method takes two parameters as emailId (the email address of the recipient) and name (the name of the user).
    It constructs an HTML formate message to be sent as an email. The message includes a greeting, congratulations message for successful registration, information about the platform's services, contact information, and a closing note.
    The HTML message is stored in the htmlTextMessage variable.
    It then attempts to send the email using a method called sendMail from a class named JavaMailUtil. This implies that there is another class called JavaMailUtil containing the necessary functionality to send emails.

--- green_life/MailMessage.java
+++ green_life/MailMessage.java
@@ -1,1 +1,24 @@
+package green_life;
 
+import jakarta.mail.MessagingException;
+
+public class MailMessage {
+	public static void registrationSuccess(String emailId, String name) {
+		String recipient = emailId;
+		String subject = "Registration Successfull";
+		String htmlTextMessage = "" + "<html>" + "<body>"
+				+ "<h2 style='color:green;'>Welcome to Green Life</h2>" + "" + "Hi " + name + ","
+				+ "<br><br>Congratulations on successfully creating your account. You're now part of our community, where shopping for your favorite products is just a click away..<br>"
+				+ "Feel free to explore our wide range of products, exclusive deals, and convenient features tailored to make your shopping experience seamless."
+				+ "<br>If you have any questions or need assistance, our support team is here to help. Just drop us a message at greenlife.notifications@gmail.com or give us a call at <b> 070 3654286 </b>.."
+				+ "<br><br>Happy shopping! "
+				+ "<br>Best regards, "
+				+ "<br>Green Life Support Team"
+				+ "Have a good day!<br>" + "" + "</body>" + "</html>";
+		try {
+			JavaMailUtil.sendMail(recipient, subject, htmlTextMessage);
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
