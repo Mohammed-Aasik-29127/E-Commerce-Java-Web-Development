32f4e1a0d3ccb8a3a7bea7142af4e666db7c41fe
Nithujayan U <159274930+NithujayanU@users.noreply.github.com>
2024-04-29 00:24:43 +0530

    Update MailMessage.java
    This Java method orderShipped is responsible for sending an email notification to a user when their order has been shipped.
    
    It takes parameters as recipientEmail, name, transId, transAmount.
    The method constructs an HTML formatted email message with the shipping details.
    It includes a greeting mentioning the recipient's name.
    It notifies the user that their order has been shipped successfully and is on the way to be delivered.
    Transaction details such as Order ID (transId) and Amount Paid (transAmount) are displayed within the email body.

--- green_life/MailMessage.java
+++ green_life/MailMessage.java
@@ -43,3 +43,25 @@ public class MailMessage {
 			e.printStackTrace();
 		}
 	}
+
+	public static void orderShipped(String recipientEmail, String name, String transId, double transAmount) {
+		String recipient = recipientEmail;
+		String subject = "Hurray!!, Your Order has been Shipped from Green Life";
+		String htmlTextMessage = "<html>" + "  <body>" + "    <p>" + "      Hey " + name + ",<br/><br/>"
+				+ "      We are glad that you shop with Green Life!" + "      <br/><br/>"
+				+ "      Your order has been shipped successfully and on the way to be delivered."
+				+ "      <br/>" + "      Here is Your Transaction Details:<br/>" + "      <br/>"
+				+ "      <font style=\"color:red;font-weight:bold;\">Order Id:</font>"
+				+ "      <font style=\"color:green;font-weight:bold;\">" + transId + "</font><br/>" + "      <br/>"
+				+ "      <font style=\"color:red;font-weight:bold;\">Amount Paid:</font> <font style=\"color:green;font-weight:bold;\">"
+				+ transAmount + "</font>" + "      <br/><br/>" + "      Thanks for shopping with us!<br/><br/>"
+				+ "      Come Shop Again! <br/<br/> <font style=\"color:green;font-weight:bold;\">Green Life.</font>"
+				+ "    </p>" + "    " + "  </body>" + "</html>";
+
+		try {
+			JavaMailUtil.sendMail(recipient, subject, htmlTextMessage);
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+	}
+
