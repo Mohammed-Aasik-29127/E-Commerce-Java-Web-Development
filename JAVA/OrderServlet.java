1711a343848e25cab26bdd959410c64f9a32e85b
sajeevanh2r <136316660+sajeevanh2r@users.noreply.github.com>
2024-04-28 09:23:55 +0530

    Update OrderServlet.java
    change the code

--- JAVA/OrderServlet.java
+++ JAVA/OrderServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.shashi.service.impl.OrderServiceImpl;
+import com.GREEN_LIFE.service.impl.OrderServiceImpl;
 
 /**
  * Servlet implementation class OrderServlet
